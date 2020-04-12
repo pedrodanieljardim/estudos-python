f4528656384f8d7f6f788ac7a849c393e9aa38da
pedrodanieljardim <pedrodanieljardim@gmail.com>
2020-04-12 11:01:57 -0300

    Adicionando exercício 18

---
+++ Curso em Vídeo/Fundamentos/ch018.py
@@ -0,0 +1,8 @@
+import math
+
+angle = int (input('Digite o Angulo: '))
+
+print('ANGULO ',angle)
+print('SENO: {:.2f}'.format(math.sin(angle)))
+print('COSSENO: {:.2f}'.format(math.cos(angle)))
+print('TANGENTE: {:.2f}'.format(math.tan(angle)))
