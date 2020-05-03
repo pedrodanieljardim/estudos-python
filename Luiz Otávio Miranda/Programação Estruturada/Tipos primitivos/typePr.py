49a5ef92e587f59da4b59d660afa83a76f390eaf
Pedro <pedro.d.jardim@hotmail.com>
2020-05-03 05:35:44 -0300

    Tipos primitivos

---
+++ Luiz Otávio Miranda/Programação Estruturada/Tipos primitivos/typePr.py
@@ -0,0 +1,23 @@
+
+## Tipos primitivos
+
+a: str = "String"
+b: int = 123
+c: float = 2352.323
+d: bool = True
+
+print("=====================================")
+print("Valor da variável: ",a)
+print(type(a))
+print("Valor da variável: ",b)
+print(type(b))
+print("Valor da variável: ",c)
+print(type(c))
+print("Valor da variável: ",d)
+print(type(d))
+print("=====================================")
+print("Pedro",type("Pedro"))
+print(10, type(10))
+print(25.23,type(25.23))
+print('l' == 'l', type('l' == 'l'))
+print("=====================================")
