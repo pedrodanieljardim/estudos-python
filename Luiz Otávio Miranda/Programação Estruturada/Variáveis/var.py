f2b5ebf984033d7a707d3dd39fa087bcf4357908
Pedro <pedro.d.jardim@hotmail.com>
2020-05-03 05:49:29 -0300

    Variáveis

---
+++ Luiz Otávio Miranda/Programação Estruturada/Variáveis/var.py
@@ -0,0 +1,10 @@
+
+nome = 'Ronaldo'
+idade = 20
+altura = 1.75
+maiorIdade = idade > 18
+
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ",altura)
+print("é maior de idade: ",maiorIdade)
