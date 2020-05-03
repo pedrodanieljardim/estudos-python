d9ef124221cb03c1efcf7bae770173396aa549d0
Pedro <pedro.d.jardim@hotmail.com>
2020-05-03 05:17:29 -0300

    Função print

---
+++ Luiz Otávio Miranda/Programação Estrutural/Função Print/funcPrint.py
@@ -0,0 +1,11 @@
+# Função Print
+
+print(123456)
+# Separação por espaço
+print('Belo Horizonte','MG')
+# Usando o parâmetro sep da função printf para separar as strings por um Caractere escolhido
+print('Contagem','MG',sep = '-')
+# outro parâmetro da função printf é o end: que possibilitar colocar algo no final da impressão...
+# por padrão, o end é prenchido por \n para quebrar a linha....
+print('Contagem','MG',sep = '-',end='#######')
+# python é case sensitive logo, Print != print
