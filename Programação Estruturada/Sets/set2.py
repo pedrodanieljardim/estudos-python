627c8190a632f40b71b52fef2e89795beaec5749
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-05-07 03:38:24 -0300

    Sets - Conversão Lista-Set

---
+++ Programação Estruturada/Sets/set2.py
@@ -0,0 +1,7 @@
+
+listaA = [1, 2, 3, 4, 5, 6, 6, 2, 3, 'Pedro', 'Pedro', 12]
+print('ListaA: {}'.format(listaA))
+listaA = set(listaA)
+print('ListaA convertida em Set : {}'.format(listaA))
+listaA = list(listaA)
+print('set ListaA convertida em Lista : {}'.format(listaA))
