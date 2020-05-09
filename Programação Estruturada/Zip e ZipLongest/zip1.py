f2ed98617ce41ba7870e2c306c5298161cf4afbf
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-05-09 04:11:32 -0300

    Zip - Sintaxe e Uso

---
+++ Programação Estruturada/Zip e ZipLongest/zip1.py
@@ -0,0 +1,8 @@
+
+cidades = ['SP','BH','SA','RJ']
+estados = ['SP','MG','BA','RJ']
+
+# une as listas em uma variável..
+estadosUnificados = zip(cidades,estados)
+
+print('Lista : {}'.format(list(estadosUnificados)))
